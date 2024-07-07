5002074dabc4fc3fe2c80cd151946ec6aa327a74
kurotamashi30 <kurotamashi30@gmail.com>
2024-07-07 23:55:04 +0900

    resr

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+
 const API_KEY = `581de57429b4469fb96dc6b412a811b2`;
-
+let newsList = []; 
 const getLatestNews = async () => {
-    const url = new URL(`${PROXY_URL}https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
-
+    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
     const response = await fetch(url);
     const data = await response.json();
     newsList = data.articles;
     render();
+};
+getLatestNews();
       //  console.log(data);
        // render(data.articles);
-};
+
 
 //프린트해봅시다.
 
 const render=()=>{
-    const newsHTML = newsList.map(
-         news=>
+    const newsHTML = newsList.map(news=>
           ` <div class ="row news">
           <div class ="col-lg-4">
               <img class="news-img-size"
@@ -29,12 +29,12 @@ const render=()=>{
               </p>
               <div>
                  ${news.source.name}*${news.publichedAt}
-    
+
           </div>`
       ).join("");
+
     document.getElementById("news-board").innerHTML =newsHTML;
   };
 //프린트해봅시다.
 
-getLatestNews();
 
