7f8607d0ab4ae990b2cef4f7c4852e7ceabbf809
kurotamashi30 <kurotamashi30@gmail.com>
2024-07-07 20:54:08 +0900

    test

--- main.js
+++ main.js
@@ -6,8 +6,9 @@ const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${AP
 try { const response = await fetch(url);
         console.log("rrr",response)
         const data = await response.json();
-        news = data.articles;
-        console.log("ddd",news);
+        newsList = data.articles;
+        console.log("ddd",newsList);
+        render();
       //  console.log(data);
        // render(data.articles);
     } catch (error) {
@@ -17,31 +18,27 @@ try { const response = await fetch(url);
 
 //프린트해봅시다.
 
-getLatestNews();
-
-//const render=(newsList)=>{
-  //  const newsHTML = ``;
-  //    newsHTML = newsList.map(
-  //        news =>
-   //       ` <div class ="row news">
-   //       <div class ="col-lg-4">
-     //         <img class="news-img-size"
-    //            src=${news.urlToImage}/>
-   //       </div>
-     //     <div class ="col-lg-8">
-      //        <h2>${news.title}</h2>
-    //          <p>
-     //             코딩 알려주는 누나 듣고 취직 완료
-//  
-       //       </p>
-      //        <div>
-      //            20249월 취직 완료
-//  //  
-      //        </div>`
- //     )
-
-//  };
+const render=()=>{
+    const newsHTML = newsList.map(
+         news=>
+          ` <div class ="row news">
+          <div class ="col-lg-4">
+              <img class="news-img-size"
+                src=${news.urlToImage}/>
+          </div>
+          <div class ="col-lg-8">
+              <h2>${news.title}</h2>
+              <p>
+                 ${news.description}
+              </p>
+              <div>
+                 ${news.source.name}*${news.publichedAt}
+    
+          </div>`
+      );
+    document.getElementById("news-board").innerHTML =newsHTML;
+  };
 //프린트해봅시다.
 
-
+getLatestNews();
 
