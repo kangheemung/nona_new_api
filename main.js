574c50b2eaa94b3b8b517d2b7630f8e24bba6063
kurotamashi30 <kurotamashi30@gmail.com>
2024-07-08 12:08:01 +0900

    test

--- main.js
+++ main.js
@@ -1,40 +1,52 @@
 
 const API_KEY = `581de57429b4469fb96dc6b412a811b2`;
-let newsList = []; 
+let newsList = [];
+
 const getLatestNews = async () => {
     const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
-    const response = await fetch(url);
-    console.log(response );
-    const data = await response.json();
-    console.log(data);
-    newsList = data.articles;
-    console.log(newsList);
-    render();
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Failed to fetch news data');
+        }
+        const data = await response.json();
+       console.log(data);
+        newsList = data.articles;
+        
+        render();
+    } catch (error) {
+        console.error('Error fetching or parsing data:', error.message);
+    }
 };
 getLatestNews();
       //  console.log(data);
-       // render(data.articles);
+      // render(data.newsList);
 
 
 //프린트해봅시다.
 
-const render=()=>{
-    const newsHTML = newsList.map(news=>
-          `<div class ="row news">
+const render = () => {
+    const newsHTML = newsList.map(news=>{
+        const imageSrc = news.urlToImage ? news.urlToImage : '<img class="news_size" src="no-pictures.png"/>';
+
+        const summary = news.description && news.description.length > 200 ? news.description.substring(0, 200) + '...' : news.description || '내용없음';
+        const sourceName = news.source.name || 'no source';
+        const publishedTime = moment(news.publishedAt, "YYYYMMDD").fromNow();
+        return `<div class ="row news">
             <div class ="col-lg-4">
-                <img class="news-img-size"
-                    src=${news.urlToImage}/>
+                <img class="news_size"
+                src=${imageSrc}/>
             </div>
           <div class ="col-lg-8">
               <h2>${news.title}</h2>
               <p>
-                 ${news.description}
+                 ${summary}
               </p>
               <div>
-                 ${news.source.name}*${news.publichedAt}
+                 ${sourceName}-${publishedTime}
               </div>
-          </div>`
-      ).join("");
+          </div>`;
+        }).join("");
 
     document.getElementById("news-board").innerHTML =newsHTML;
   };
